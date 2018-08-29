03e9e047ea20d17abca4731d1a5dee8c4e5ed3be
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-28 23:04:21 -0300

    Initial commit

---
+++ app/src/main/java/br/com/adriano/primeiroprojeto/FormularioActivity.kt
@@ -0,0 +1,13 @@
+package br.com.adriano.primeiroprojeto
+
+import android.app.Activity
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FormularioActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario)
+    }
+}
